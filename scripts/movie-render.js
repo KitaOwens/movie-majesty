e56fbcf2a8c15729ae0090ac607005de5f75d438
Kita Owens <owens.coding@proton.me>
2026-09-13 17:25:35 -0500

    Added HTML rendering module

--- scripts/movie-render.js
+++ scripts/movie-render.js
@@ -1,19 +1,9 @@
-import { movies } from './movies-data.js';
+export const renderMoviesToDOM = (movieHTML) => {
+   const movieList = document.getElementById('movie-list');
 
-export const generateMoviesHTML = () => {
-   let movieHTML = '';
-
-   for (const movie of movies) {
-       movieHTML += `
-           <article class="movie">
-               <img src="${movie.poster}" alt="${movie.title} poster" class="movie__poster">
-               <div class="movie__details">
-                   <h2 class="movie__title">${movie.title}</h2>
-                   <p class="movie__description">${movie.description}</p>
-               </div>
-           </article>
-       `;
+   if (movieList) {
+       movieList.innerHTML = movieHTML;
+   } else {
+       console.error('Could not find element with id "movie-list"');
    }
-
-   return movieHTML
 };
